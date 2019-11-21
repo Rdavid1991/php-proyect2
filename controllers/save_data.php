8144762df4bcb2fdbac419089b1325e1430e7ded
rdavid <rdavid3.1991@gmail.com>
2019-11-21 15:02:48 -0500

    List of productos

--- controllers/save_data.php
+++ controllers/save_data.php
@@ -1,9 +1,7 @@
 <?php
-
-include("class_lib.php");
+include("../class/save_image.php");
 
 if (is_uploaded_file($_FILES['imagen']['tmp_name']) && array_key_exists('nombre', $_POST)) {
     $img = new Save_Data();
     $img->save($_FILES,$_POST);
-    echo $_POST['nombre'];
 }
